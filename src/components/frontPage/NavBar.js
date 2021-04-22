2dcb0f0066b1d1292a99e9851940b8ca6f4b48b1
larbikhounti <larbikhounti@gmail.com>
2021-04-22 15:47:41 -0700

    remove drop downs from nav

--- src/components/frontPage/NavBar.js
+++ src/components/frontPage/NavBar.js
@@ -34,32 +34,7 @@ function NavBar(props) {
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
                 {/*<Nav.Link href="#home">les nouveaux </Nav.Link>*/}
-                <NavDropdown title="Apparel" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    Something
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
-                </NavDropdown>
-                <NavDropdown title="Accessories" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    Something
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
-                </NavDropdown>
+                
               </Nav>
             </Navbar.Collapse>
           </Col>
